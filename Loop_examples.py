f30af9bd4bccd43e4c74e70df3f3042302df9b4c
Ryan Bekins <Ryan.S.Bekins@gmail.com>
2023-03-28 23:27:49 -0400

    loop examples

---
+++ Loop_examples.py
@@ -0,0 +1,24 @@
+# new_array = [1, 2766, 3, 4, 5, 65445, 799, 8766]
+# # for <indexing variable> in < array/list>
+# array_sum = 0
+# print(new_array)
+# for element in new_array:
+#     print("element", element)
+#     array_sum = array_sum + element
+#     print("array_sum", array_sum)
+# print("world")
+# print(array_sum)
+
+random_str = "random_str"
+double_str = ""
+for char in random_str:
+    print("char", char * 3)
+    double_str = double_str + char + char
+# print("double string", double_str)
+# # for i in range(2, 10):
+# #     print(i)
+# # for i in range(10):
+# #     print(i)
+# for i in range(len(random_str)):
+#     print(i)
+#     print(random_str[i])
